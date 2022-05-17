5b335d945a2b1b6ba1b110a19e10b461445d76eb
kishan_vekariya <kvekariya@tibicle.com>
2022-05-17 12:54:42 +0530

    first commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:  const [
+          Text('Hello World', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),), SizedBox(height: 30,),
+          Text('Hello World', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+        ],
+      ),
+    );
+  }
+}
